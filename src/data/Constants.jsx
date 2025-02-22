6e8ffdafa213e43faeb95516c416461d0f7ef7a0
Akshita <akshita.sharma11297@gmail.com>
2025-02-22 17:38:43 -0500

    GH links

--- src/data/Constants.jsx
+++ src/data/Constants.jsx
@@ -4,5 +4,5 @@ export const description =
   "I am currently pursuing MS in Cybersecurity from the University of North Carolina at Charlotte. I am passionate about Cybersecurity, SIEM/SOC, DLP, security and software technologies.";
 export const email = "asharm88@uncc.edu";
 export const linkedIn = "https://www.linkedin.com/in/akshita-sharma01/";
-export const github = "https://github.com/Aks-sh/Akshita-Sharma-Portfolio/README.md";
-export const resume = "https://github.com/Aks-sh/Akshita-Sharma-Portfolio/my-website/assets/resume/Akshita_resume.pdf";
+export const github = "https://github.com/akshita-x/Akshita-Sharma-Portfolio-Readme";
+export const resume = "https://github.com/akshita-x/akshita-x.github.io/blob/67f078b527bac56bfa72e6902009ceffb857d90b/assets/resume/Akshita_resume.pdf";
